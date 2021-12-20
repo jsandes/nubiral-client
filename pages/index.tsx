fe63caa23bf86208cf996f4e47c47ab04cd49e29
marcosirb <marcosrissotto@gmail.com>
2021-12-20 16:14:05 -0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="home">
+      <h1>Desde home</h1>
+    </div>
+  )
+}
+
+export default Home;
