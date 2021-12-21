bb0a8afba4ac07c67a4870183fd95ba387aa84c1
marcosirb <marcosrissotto@gmail.com>
2021-12-20 22:34:15 -0300

    Update layout

---
+++ components/Header/index.tsx
@@ -0,0 +1,10 @@
+import TopBar from './TopBar';
+
+export default function index() {
+  return (
+    <div className="header">
+      <TopBar />
+      
+    </div>
+  )
+}
--- layouts/BasicLayout/index.ts
+++ layouts/BasicLayout/index.ts
@@ -1,1 +1,1 @@
-export { default } from './BasicLayout.component';
+export { default } from './BasicLayout';
