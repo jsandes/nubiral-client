60370588b7a6b52e29fcc566d076148a10f687fb
marcosirb <marcosrissotto@gmail.com>
2021-12-21 10:53:19 -0300

    Update table component with hosts

--- pages/api/hosts.ts
+++ pages/api/hosts.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 
 export async function getHostApi() {
   const BASE_PATH = 'http://localhost:4000/api/products/host';
-  const borrarToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWJmZTk4NTk0NmUyZDEzZTMxZThiYmMiLCJpYXQiOjE2Mzk5NjcxMDl9.3xTj_2XRIW6xhyTqxP6ib8PvPrfZ6fQibO6LeTfLFkU';
+  const borrarToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWJmZTk4NTk0NmUyZDEzZTMxZThiYmMiLCJpYXQiOjE2NDAwODg0MjksImV4cCI6MTY0MDE3NDgyOX0.F5e2VRBGb06zigvbP5gOafTtW79JdYxawl3qcS0rZOQ';
   console.log('BASE_PATH',BASE_PATH);
   try {
     const url = BASE_PATH;
@@ -12,7 +12,6 @@ export async function getHostApi() {
         authorization: borrarToken 
       }
     });
-    console.log('resp data', data.result);
     return data.result;
   } catch(err){
     console.log(err);
