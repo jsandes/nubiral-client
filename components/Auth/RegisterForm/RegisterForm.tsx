01523aba1307cbde666e2101e01eab1b52acb0c0
marcosirb <marcosrissotto@gmail.com>
2021-12-22 18:25:11 -0300

    Update register form

--- components/Auth/RegisterForm/RegisterForm.tsx
+++ components/Auth/RegisterForm/RegisterForm.tsx
@@ -1,14 +1,68 @@
-import { Button } from 'semantic-ui-react';
+import { Form, Button} from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { ObjectShape } from 'yup/lib/object';
 
 interface Props {
   showLoginForm: Function;
 }
 
 export default function LoginForm({showLoginForm}: Props) {
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema:Yup.object(validationSchema),
+    onSubmit: (formData) => {
+      console.log(formData)
+    }
+  })
+
   return (
     <div>
-     <h1>RegisterForm</h1>
-     <Button onClick={showLoginForm}>Login</Button>
+      <h2>Registro</h2>
+      <Form className="login-form" onSubmit={formik.handleSubmit}>
+        <Form.Input 
+          name="username"
+          type="text"
+          placeholder="Username"
+          onChange={formik.handleChange}
+          error={formik.errors.username}
+        />
+        <Form.Input 
+          name="email"
+          type="text"
+          placeholder="Email"
+          onChange={formik.handleChange}
+          error={formik.errors.email}
+        />
+        <Form.Input 
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={formik.handleChange}
+          error={formik.errors.password}
+        />
+        <div className="actions">
+          <Button onClick={showLoginForm}>Iniciar sesión</Button>
+          <Button type="submit">Registrar</Button>
+        </div>
+      </Form>
     </div>
   )
+}
+
+function initialValues(){
+  return {
+    username: '',
+    email: '',
+    password: ''
+  }
+}
+
+function validationSchema(){
+  return {
+    username: Yup.string().required(true),
+    email: Yup.string().required(true),
+    password: Yup.string().required(true),
+  }
 }
